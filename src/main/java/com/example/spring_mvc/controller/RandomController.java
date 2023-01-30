08e63c8b30087abe112e8cedadb42d90b469b57a
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-30 16:18:17 +0100

    add task1 spring parameters

--- src/main/java/com/example/spring_mvc/controller/RandomController.java
+++ src/main/java/com/example/spring_mvc/controller/RandomController.java
@@ -3,6 +3,7 @@ package com.example.spring_mvc.controller;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class RandomController {
     @RequestMapping("/show-random")
     @ResponseBody
-    public int showRandom(){
+    public int showRandom() {
         Random r = new Random();
         return r.nextInt(100 - 1) + 1;
+    }
+
+    @RequestMapping("/random/{max}")
+    @ResponseBody
+    public int randomMax(@PathVariable int max) {
+        Random r = new Random();
+        return r.nextInt(max - 1) + 1;
     }
 }
